dd7bd9cca58da8f55bcd18c7b8a84b44f32fabf8
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-08-03 17:38:47 +0530

    refactor

--- block-BNaaau/index.js
+++ block-BNaaau/index.js
@@ -1,34 +1,32 @@
 var http = require('http');
+var fs = require('fs')
+var url = require('url')
+var PORT = 2345
 
 var server = http.createServer(handle)
 function handle(req,res){
-   res.end("welcome")
+    var parsedUrl=  url.parse(req.url , true)
+    console.log(parsedUrl.pathname,req.url)
+    res.setHeader('content-type' , "application/json")
+    res.end(JSON.stringify(parsedUrl.query))
 }
 
-server.listen("5000" , () => {
-    console.log("port 5000")
+server.listen(PORT , () => {
+    console.log("listening port" + PORT)
 })
 
 
-// var http = require("http");
-// var url = require('url')
 
-// var server = http.createServer(handle);
+
+// var http = require('http');
+
+// var server = http.createServer(handle)
 // function handle(req,res){
-//     var parsedUrl = url.parse(req.url)
-//     var pathname = parsedUrl.pathname
-//    if(req.method === "GET" && req.url === "/"){
-//        res.write("Welcome to index page")
-//        res.end()
-//    }else if(req.method === "GET" && pathname === "/about"){
-//        res.setHeader('Content-Type' , 'text/html')
-//        res.end('<h1>About Page</h1>')
-//    }else{
-//        res.writeHead(404, {'Content-Type' : "text/html"})
-//        res.end("<h1>Page not Found</h1>")
-//    }
+//    res.end("welcome")
 // }
 
-// server.listen(3000 , () => {
-//     console.log("server list port 3000")
+// server.listen("5000" , () => {
+//     console.log("port 5000")
 // })
+
+
