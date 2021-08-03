f403e032146eed2d19c95d3f06e14bd856375fa4
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-08-03 12:13:28 +0530

    refactor

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,11 +3,12 @@ var http = require("http")
 var server = http.createServer(handle)
 
 function handle(req,res){
+    console.log(req.method)
     res.statusCode = 201
-    res.setHeader("content-type" , "text/html")
+    res.setHeader("Content-type" , "text/html")
     res.end("<h1>welcome</h1>")
 }
 
-server.listen(4000,() => {
+server.listen(4444,() => {
     console.log("server listening on post")
 })
